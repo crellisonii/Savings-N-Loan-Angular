c1095dcd1580758d386a9c021badb39489e8a631
Charles Ellison <crelliso@gmail.com>
2024-10-24 20:33:39 -0500

    add enums file

---
+++ src/app/formulas/enums.formula.ts
@@ -0,0 +1,15 @@
+export enum SimpleInterestFrequency {
+  YEARLY = 1,
+  MONTHLY = 12,
+}
+
+export enum LengthType {
+  YEARLY = 'yearly',
+  MONTHLY = 'monthly',
+}
+
+export enum CompoundFrequency {
+  YEARLY = 1,
+  MONTHLY = 12,
+  DAILY = 365,
+}
